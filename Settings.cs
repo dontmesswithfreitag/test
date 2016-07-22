0a5583d7e0788b71a84b8a768679da9e519573cb
dontmesswithfreitag <dontmesswithfreitag@yahoo.de>
2016-07-22 12:03:49 +0200

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace freNWTools.Internals
+{
+    public class Settings
+    {
+        public static bool  ShowMobs
+        { 
+            get { return Properties.Settings.Default.ShowMobs; }
+            set
+            {
+                Properties.Settings.Default.ShowMobs = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+
+        public static bool ShowLoot
+        {
+            get { return Properties.Settings.Default.ShowLoot; }
+            set
+            {
+                Properties.Settings.Default.ShowLoot = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        public static bool ShowPets
+        {
+            get { return Properties.Settings.Default.ShowPets; }
+            set
+            {
+                Properties.Settings.Default.ShowPets = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        public static bool ShowNPC
+        {
+            get { return Properties.Settings.Default.ShowNPC; }
+            set
+            {
+                Properties.Settings.Default.ShowNPC = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        public static bool Show { get; set; }
+
+
+     
+    }
+}
